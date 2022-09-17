4f8cbaa6cb9be3159084cae87ea00d71a833c2e3
Abdullah Alhariry <abdullahalhariry@gmail.com>
2022-09-17 20:48:02 +0300

    End of section 8

--- API/DTOs/memberDto.cs
+++ API/DTOs/memberDto.cs
@@ -2,31 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Extensions;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class memberDto
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public DateTime DateOfBirthy { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime Created {get; set;} = DateTime.Now;
-        public DateTime LastActive {get; set;} = DateTime.Now;
+        public DateTime Created {get; set;}
+        public DateTime LastActive {get; set;}
         public string Gender {get; set;}
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string Country {get; set;}
         public string City { get; set; }
-        public ICollection<Photo> Photos { get; set; }
-
-       /* public int GetAge()
-        {
-            return DateOfBirthy.CalculateAge();
-        }*/
+        public ICollection<PhotoDto> Photos { get; set; }
     }
 }
---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUserById(int id);
+        Task<AppUser> GetUserByUserName(string username);
+
+        Task<IEnumerable<memberDto>> GetMembersAsync();
+        Task<memberDto> GetMemberAsync(string username);
+    }
+}
